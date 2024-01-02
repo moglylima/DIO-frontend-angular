6891be32316aae0d321ce05d760dde2b5f5762b3
Mogleson Maciel <moglesonlima@gmail.com>
2024-01-02 07:27:20 -0300

    doc: Incializando versionamento dos codigos implementados durante a formação.

---
+++ basic-front-end/javascript-developer-m1/README.md
@@ -0,0 +1,3 @@
+# JavaScript Developer: Módulo I
+
+Repositório de Referência do Módulo I da Trilha "JavaScript Developer" 
---
+++ basic-front-end/javascript-developer-m1/test.js
@@ -0,0 +1,7 @@
+const numerosPares = [];
+
+for (let i = 0; i < 10; i = i + 2) {
+    numerosPares.push(i);
+}
+
+console.log(numerosPares);
