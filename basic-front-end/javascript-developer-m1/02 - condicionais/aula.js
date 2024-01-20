037cecfdad6c3ffdff21ceb70ca33d704b005424
Mogleson Lima <moglesonlima@gmail.com>
2024-01-20 12:56:50 -0300

    Delete basic-front-end directory

--- basic-front-end/javascript-developer-m1/02 - condicionais/aula.js
+++
@@ -1,9 +0,0 @@
-
-const numero = 5;
-const ehNumeroPar = (numero % 2) === 0;
-
-if (ehNumeroPar) {
-    console.log('O número é par');
-} else {
-    console.log('O número é impar');
-}
--- basic-front-end/javascript-developer-m1/README.md
+++
@@ -1,3 +0,0 @@
-# JavaScript Developer: Módulo I
-
-Repositório de Referência do Módulo I da Trilha "JavaScript Developer" 
