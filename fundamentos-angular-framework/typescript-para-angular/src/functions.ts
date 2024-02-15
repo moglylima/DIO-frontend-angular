a8e102f2bf4b729c8d09e6c01e040f62abe6ded7
Mogleson Maciel <moglesonlima@gmail.com>
2024-02-15 14:51:15 -0300

    doc: Adição de um README com um breve resumo acerca dos assuntos abordados e com algumas explicações acerca de TS.

--- fundamentos-angular-framework/typescript-para-angular/src/functions.ts
+++ fundamentos-angular-framework/typescript-para-angular/src/functions.ts
@@ -12,7 +12,7 @@ function CallToPhone(phone: number | string): number | string {
 }
 
 async function getDatabase(id: number): Promise<string> {
-  return "felipe";
+  return "mogleson";
 }
 
 let soma: number = addNumber(4, 7);
